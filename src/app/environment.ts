93e5d08dd57578c160b13b47f31bb6786e73f166
Jose Pablo Valenzuela Miramontes <pabloeng@Mac-mini.local>
2023-05-20 22:54:28 -0600

    fix: Removed process.env object

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -38,19 +38,9 @@ const images: Image[] = [
 export const environment = {
     production: false,
     images,
-    // FB_CONFIG: {
-    //     apiKey: process.env['todo.api_key'],
-    //     authDomain: process.env['todo.auth_domain'],
-    //     projectId: process.env['todo.project_id'],
-    //     storageBucket: process.env['todo.storage_bucket'],
-    //     messagingSenderId: process.env['todo.messaging_sender_id'],
-    //     appId: process.env['todo.app_id'],
-    //     measurementId: process.env['todo.measurement_id'],
-    // },
     FB_DEVTOOLS_CONFIG: {
         maxAge: 25,       // Retains last 25 states
         logOnly: false,   // Restrict extension to log-only mode
         autoPause: true,  // Pauses recording actions and state changes when the
-        // extension window is not open
     },
 };
